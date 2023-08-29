c1cd3d4d61ecc08a90b34cb219504309a7c2a635
Tanya <94377598+learnertanya@users.noreply.github.com>
2023-08-30 01:37:23 +0530

    branch-change

--- src/components/EditDoc/index.tsx
+++ src/components/EditDoc/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React,{useState,useRef, useEffect} from "react";
 import './index.scss'
 import {BiArrowBack} from 'react-icons/bi'
+import ReactQuill from 'react-quill';
 
 export default function EditDoc({handleEdit}: functioninterface){
-    return <div onClick={handleEdit}><BiArrowBack/></div>
-}
+    let quillRef= useRef<any>(null);
+    const [value, setValue] = useState('');
+    useEffect(()=>{
+      quillRef.current.focus();
+    },[])
+    return (
+     <div >
+        <BiArrowBack onClick={handleEdit} size={30} className="react-icon"/>
+        <div  className="quil-container">
+        <ReactQuill className="react-quill"
+        theme="snow"
+        ref={quillRef}
+         value={value} 
+         onChange={setValue} />
+
+        </div>
+        </div>
+    )
+} 
